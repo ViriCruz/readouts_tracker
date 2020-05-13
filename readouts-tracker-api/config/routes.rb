b9aa28eaea566573356d1c0e29b98a14985a6571
ViriCruz <viridiana.cruz.olivera@gmail.com>
2020-05-13 15:57:16 -0700

    Add namespaces for versioning the api

--- readouts-tracker-api/config/routes.rb
+++ readouts-tracker-api/config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  resources :measurements
-  resources :readings
-  resources :categories
-  resources :users
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :measurements
+      resources :readings
+      resources :categories
+      resources :users
+    end
+  end
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
