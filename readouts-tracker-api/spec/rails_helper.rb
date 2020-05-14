f8ec178591bb05488cfb0b16297a188971331600
ViriCruz <viridiana.cruz.olivera@gmail.com>
2020-05-13 19:19:05 -0700

    Add support files config

--- readouts-tracker-api/spec/rails_helper.rb
+++ readouts-tracker-api/spec/rails_helper.rb
@@ -21,7 +21,7 @@ require 'rspec/rails'
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-# Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
+Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
@@ -79,6 +79,7 @@ RSpec.configure do |config|
   #       # ...
   #     end
   #
+  config.include RequestSpecHelper, type: :request
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
