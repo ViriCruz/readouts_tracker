d412310429d0fc61896a4d93dafdabad20ab8dc5
ViriCruz <viridiana.cruz.olivera@gmail.com>
2020-05-15 14:48:47 -0700

    Refactor reading factory

--- readouts-tracker-api/spec/factories/readings.rb
+++ readouts-tracker-api/spec/factories/readings.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :reading do
     description { Faker::Lorem.word }
-    duration { Faker::Time }
+    duration { Faker::Time.backward(days: 5, period: :morning, format: :short) }
   end
 end
