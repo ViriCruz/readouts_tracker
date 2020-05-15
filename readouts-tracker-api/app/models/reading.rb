3f19c290643b2a37293e2aab84ad7dc9959eba94
ViriCruz <viridiana.cruz.olivera@gmail.com>
2020-05-15 14:52:23 -0700

    Remove validates presence of created_at attribute

--- readouts-tracker-api/app/models/reading.rb
+++ readouts-tracker-api/app/models/reading.rb
@@ -3,5 +3,5 @@ class Reading < ApplicationRecord
   belongs_to :category
 
   # validations
-  validates_presence_of :description, :duration, :created_at
+  validates_presence_of :description, :duration
 end
