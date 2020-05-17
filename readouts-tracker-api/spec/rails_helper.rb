2375fa992a3a3889083643228e367743e381d5f6
ViriCruz <viridiana.cruz.olivera@gmail.com>
2020-05-16 17:40:19 -0700

    Modify rails_helper when importing support files

--- readouts-tracker-api/spec/rails_helper.rb
+++ readouts-tracker-api/spec/rails_helper.rb
@@ -79,7 +79,8 @@ RSpec.configure do |config|
   #       # ...
   #     end
   #
-  config.include RequestSpecHelper, type: :request
+  config.include RequestSpecHelper
+  config.include ControllerSpecHelper
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
