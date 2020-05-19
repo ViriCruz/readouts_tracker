1f0c23846bc4b2dd1b4756d2aa9328b01a484e6a
ViriCruz <viridiana.cruz.olivera@gmail.com>
2020-05-19 15:56:18 -0700

    Remove unnecessary files and add enzyme to setupTests.js

--- readouts_tracker_app/src/index.js
+++ readouts_tracker_app/src/index.js
@@ -1,15 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import { applyMiddleware, createStore } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers/index';
+import App from './components/app'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+const middlewares = [thunk];
+
+const initialState = {
+  pending: true,
+  data: [],
+  error: null,
+};
+
+const store = createStore(rootReducer, {
+  user: initialState,
+  categories: initialState,
+  readings: initialState,
+  measurements: initialState
+}, applyMiddleware(...middlewares));
 
 ReactDOM.render(
-  <React.StrictMode>
-    <div>Night night burn</div>
-  </React.StrictMode>,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
