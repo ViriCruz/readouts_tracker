546d3ddbf387d2451e77420ea0bec6541339c8ee
ViriCruz <viridiana.cruz.olivera@gmail.com>
2020-05-19 09:02:01 -0700

    Add redux actions and remove unnecessary files

--- readouts_tracker_app/src/index.js
+++ readouts_tracker_app/src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <div>Night night burn</div>
   </React.StrictMode>,
   document.getElementById('root')
 );
