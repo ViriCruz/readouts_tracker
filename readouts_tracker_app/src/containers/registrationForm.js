ebd58f9889ec6edf2e9e4d74cc6bd42f8253c81c
ViriCruz <viridiana.cruz.olivera@gmail.com>
2020-05-20 00:22:26 -0700

    Add bootstrap classes to registration form component

--- readouts_tracker_app/src/containers/registrationForm.js
+++ readouts_tracker_app/src/containers/registrationForm.js
@@ -18,7 +18,7 @@ class RegistrationForm extends React.Component {
     this.handleChange = this.handleChange.bind(this);
   }
 
-  handleSubmit() {
+  handleSubmit(event) {
     const { firstName, lastName, email, password, password_confirmation } = this.state;
     console.log(
       firstName,
@@ -27,7 +27,7 @@ class RegistrationForm extends React.Component {
       password,
       password_confirmation
     );
-    
+    event.preventDefault();
   }
 
   handleChange(event) {
@@ -39,54 +39,79 @@ class RegistrationForm extends React.Component {
 
   render () {
     return (
-      <div>
-        <form onSubmit={this.handleSubmit}>
-          <input
-            type="text"
-            name="firstName"
-            placeholder="Your first name"
-            value={this.state.firstName}
-            onChange={this.handleChange}
-            required
-          />
+      <div className="vh-100 d-flex flex-column align-items-center justify-content-center">
+        <div className="d-flex justify-content-center">
+          <h1 className="h3">Sign Up Form</h1>
+        </div>
+        <form 
+          className="d-flex flex-column justify-content-center align-items-center" 
+          onSubmit={this.handleSubmit}
+        >
+          <div className="form-group col-sm-12">
+            <input
+              className="form-control"
+              type="text"
+              name="firstName"
+              placeholder="Your first name"
+              value={this.state.firstName}
+              onChange={this.handleChange}
+              required
+            />
+          </div>
+          <div className="form-group col-sm-12">
+            <input
+              className="form-control"
+              type="text"
+              name="lastName"
+              placeholder="Your last name"
+              value={this.state.lastName}
+              onChange={this.handleChange}
+              required
+            />
+          </div>
 
-          <input
-            type="text"
-            name="lastName"
-            placeholder="Your last name"
-            value={this.state.lastName}
-            onChange={this.handleChange}
-            required
-          />
+          <div className="form-group col-sm-12">
+            <input
+              className="form-control"
+              type="email"
+              name="email"
+              placeholder="Email"
+              value={this.state.email}
+              onChange={this.handleChange}
+              required
+            />
+          </div>
 
-          <input
-            type="email"
-            name="email"
-            placeholder="Email"
-            value={this.state.email}
-            onChange={this.handleChange}
-            required
-          />
+          <div className="form-group col-sm-12">
+            <input
+              className="form-control"
+              type="password"
+              name="password"
+              placeholder="Password"
+              value={this.state.password}
+              onChange={this.handleChange}
+              required
+            />
+          </div>
 
-          <input
-            type="password"
-            name="password"
-            placeholder="Password"
-            value={this.state.password}
-            onChange={this.handleChange}
-            required
-          />
+          <div className="form-group col-sm-12">
+            <input
+              className="form-control"
+              type="password"
+              name="password_confirmation"
+              placeholder="Password confirmation"
+              value={this.state.password_confirmation}
+              onChange={this.handleChange}
+              required
+            />
+          </div>
 
-          <input
-            type="password"
-            name="password_confirmation"
-            placeholder="Password confirmation"
-            value={this.state.password_confirmation}
-            onChange={this.handleChange}
-            required
-          />
-
-          <button type="submit">Register</button>
+          <div className="form-group row">
+            <div className="col-sm-10">
+              <button type="submit" className="btn btn-primary">Register</button>
+            </div>
+          </div>
+          
         </form>
       </div>
     )
--- readouts_tracker_app/src/index.js
+++ readouts_tracker_app/src/index.js
@@ -8,9 +8,6 @@ import App from './components/app'
 import './styles/reset.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import Register from './api/registerUser'
-
-
 const middlewares = [thunk];
 
 const initialState = {
@@ -32,25 +29,4 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 );
-
-const test = async() => {
-  const response = await Register.createUser({
-    first_name: 'user from react',
-    last_name:'doe',
-    email:'john@example.com',
-    password:"1234",
-    password_confirmation:"1234"
-  })
-
-  console.log(response)
-}
-
-
-test()
-
-
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 
