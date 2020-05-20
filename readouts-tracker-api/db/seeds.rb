345980b05ad3b44339918669f0aa96c094fc3134
ViriCruz <viridiana.cruz.olivera@gmail.com>
2020-05-19 19:27:51 -0700

    Add seed to create categories

--- readouts-tracker-api/db/seeds.rb
+++ readouts-tracker-api/db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Category.create!(
+  name: "books"
+)
+
+Category.create!(
+  name: "audiobooks"
+)
+
+Category.create!(
+  name: "articles"
+)
+
+Category.create!(
+  name: "mangas"
+)
+
+Category.create!(
+  name: "comics"
+)
