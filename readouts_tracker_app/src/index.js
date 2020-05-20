278e5ed3c886c6ca99fa543762562f6d5a73668e
ViriCruz <viridiana.cruz.olivera@gmail.com>
2020-05-19 17:56:17 -0700

    Add barebone code

--- readouts_tracker_app/src/index.js
+++ readouts_tracker_app/src/index.js
@@ -5,7 +5,10 @@ import { applyMiddleware, createStore } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers/index';
 import App from './components/app'
+import './styles/reset.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Register from './api/registerUser'
 
 
 const middlewares = [thunk];
@@ -29,6 +32,23 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 );
+
+const test = async() => {
+  const response = await Register.createUser({
+    first_name: 'user from react',
+    last_name:'doe',
+    email:'john@example.com',
+    password:"1234",
+    password_confirmation:"1234"
+  })
+
+  console.log(response)
+}
+
+
+test()
+
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
