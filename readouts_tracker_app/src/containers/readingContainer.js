5cbe945e1c6eb3a0e18436eb46b5a66e97a5c6dd
viricruz <viridiana.cruz.olivera@gmail.com>
2020-05-31 12:42:19 -0700

    Add totalTime to state

--- readouts_tracker_app/src/containers/readingContainer.js
+++ readouts_tracker_app/src/containers/readingContainer.js
@@ -3,8 +3,10 @@ import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux';
 import { getCategory } from '../reducers/categoryReducer';
 import TrackReading from '../components/trackReading'
-import Reading from '../api/createReading'
+import Reading from '../api/createReading';
+import TotalTime from  '../api/fetchTotalTime';
 import { getReadings, getReadingsError, getReadingsPending } from '../reducers/readingReducer'
+import { getTotalTime, getTotalTimeError, getTotalTimePending } from '../reducers/totalTimeReducer'
 import { Redirect } from 'react-router';
 
 const mapStateToProps = state => ({
@@ -13,11 +15,17 @@ const mapStateToProps = state => ({
     pending: getReadingsPending(state.readings),
     data: getReadings(state.readings),
     error: getReadingsError(state.readings)
+  },
+  totalTime: {
+    pending: getTotalTimePending(state.totalTime),
+    data: getTotalTime(state.totalTime),
+    error: getTotalTimeError(state.totalTime)
   }
 })
 
 const mapDispatchToProps = dispatch => bindActionCreators({
   pushReading: Reading.pushReading,
+  totalTime: TotalTime.fetchTotalTime
 }, dispatch)
 
 class ReadingContainer extends React.Component {
@@ -37,12 +45,17 @@ class ReadingContainer extends React.Component {
 
   setDuration(hours, minutes, event){
     const { preventDefault, target } = event
-    
+    const { totalTime } = this.props
+    const { id } = this.props.category
+    const token = localStorage.getItem('__token__')
+    const today = new Date();
+    const day = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
     this.setState({
       hours: hours,
       minutes: minutes
     }, () => {
       this.handleSave({target, preventDefault})
+      totalTime(token, id, day)
     })
   }
 
@@ -71,14 +84,23 @@ class ReadingContainer extends React.Component {
   render() {
     const { description } = this.state
     const { category } = this.props
+    const { data } = this.props.readings
     if(!category) return <Redirect to='/categories' />
+
     return(
-      <TrackReading 
-        handleSave={this.handleSave}  
-        setDescription={this.setDescription} 
-        duration={this.setDuration}
-        value={description}
-      />
+      <div>
+        <TrackReading 
+          handleSave={this.handleSave}  
+          setDescription={this.setDescription} 
+          duration={this.setDuration}
+          value={description}
+        />
+        <div 
+          className={'id' in data ? 'alert alert-success d-block' : 'd-none'}>
+          Saved!
+        </div> 
+        
+      </div>
     )
   }
 }
