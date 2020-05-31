a72ad6779e6ec3314cd9d9a5f5e65645bd37dfc1
viricruz <viridiana.cruz.olivera@gmail.com>
2020-05-31 12:43:02 -0700

    Add totalTime to combineReducers

--- readouts_tracker_app/src/reducers/index.js
+++ readouts_tracker_app/src/reducers/index.js
@@ -4,12 +4,14 @@ import { measurementsReducer } from './measurementReducer'
 import { readingsReducer } from './readingReducer'
 import { categoriesReducer } from './categoriesReducer'
 import { categoryReducer } from './categoryReducer'
+import { totalTimeReducer } from './totalTimeReducer';
 
 const rootReducer = combineReducers({
   user: usersReducer,
   categories: categoriesReducer,
   readings: readingsReducer,
   measurements: measurementsReducer,
+  totalTime: totalTimeReducer,
   category: categoryReducer
 });
 
