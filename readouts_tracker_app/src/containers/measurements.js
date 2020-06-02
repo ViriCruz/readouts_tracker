191710919409e1d01a3b5da824cd996ea977818d
ViriCruz <viridiana.cruz.olivera@gmail.com>
2020-06-01 17:22:18 -0700

    Add pie chart

--- readouts_tracker_app/src/containers/measurements.js
+++ readouts_tracker_app/src/containers/measurements.js
@@ -1,19 +1,29 @@
 import React from 'react'
-import {Radar, RadarChart, PolarGrid, Legend,
-  PolarAngleAxis, PolarRadiusAxis} from 'recharts';
+import {PieChart, Pie, Sector, Cell} from 'recharts';
+
 import Measurements from '../api/fetchMeasurements';
 import { getMeasurements, getMeasurementsError, getMeasurementsPending } from '../reducers/measurementReducer'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 
-const data = [
-    { subject: 'books', A: 120, B: 110, fullMark: 20 },
-    { subject: 'audiobooks', A: 98, B: 130, fullMark: 150 },
-    { subject: 'articles', A: 86, B: 130, fullMark: 150 },
-    { subject: 'manga', A: 99, B: 100, fullMark: 150 },
-    { subject: 'comics', A: 85, B: 90, fullMark: 150 },
-    
-];
+const data = [{name: 'Group A', value: 1.4}, {name: 'Group B', value: 1.59},
+                  {name: 'Group C', value: .6}, {name: 'Group D', value: 2.05}];
+
+const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
+
+const RADIAN = Math.PI / 180;                    
+const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }) => {
+  const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+const x  = cx + radius * Math.cos(-midAngle * RADIAN);
+const y = cy  + radius * Math.sin(-midAngle * RADIAN);
+
+return (
+  <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} 	dominantBaseline="central">
+    {`${(percent * 100).toFixed(0)}%`}
+  </text>
+);
+};              
+
 
 const mapStateToProps = state => ({
   measurements: {
@@ -29,6 +39,21 @@ const mapDispatchToProps = dispatch => bindActionCreators({
 
 class Measure extends React.Component {
 
+  constructor(props){
+    super(props)
+    this.state = {
+      activeIndex: 0
+    }
+
+    this.onPieEnter = this.onPieEnter.bind(this)
+  }
+
+  onPieEnter(index){
+    this.setState({
+      activeIndex: index
+    })
+  }
+
   componentDidMount(){
     const { fetchMeasurements } = this.props
     const token = localStorage.getItem('__token__')
@@ -37,12 +62,23 @@ class Measure extends React.Component {
   
   render () {
     return (
-      <RadarChart cx={150} cy={200} outerRadius={70} width={600} height={500} data={data}>
-        <PolarGrid />
-        <PolarAngleAxis dataKey="subject" />
-        <PolarRadiusAxis/>
-        <Radar name="Mike" dataKey="B" stroke="#8884d8" fill="#8884d8" fillOpacity={0.6}/>
-      </RadarChart>
+      <div className="d-flex justify-content-center align-items-center">
+        <PieChart width={800} height={400} onMouseEnter={this.onPieEnter}>
+        <Pie
+          data={data} 
+          cx={400} 
+          cy={200} 
+          labelLine={false}
+          label={renderCustomizedLabel}
+          outerRadius={80} 
+          fill="#8884d8"
+        >
+        	{
+          	data.map((_entry, index) => <Cell fill={COLORS[index % COLORS.length]}/>)
+          }
+        </Pie>
+      </PieChart>
+       </div>
     );
   }
 }
